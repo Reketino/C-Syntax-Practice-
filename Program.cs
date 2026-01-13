3125ff20f84a583cd40e11a956d6e55dbd099607
Reketino <bjornevensk8@gmail.com>
2026-01-13 17:16:40 +0100

    Hope someone else become just as good of a friend w/ the console as I did today🫂

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hey what's ur name");
+string? name = Console.ReadLine();
+
+if(string.IsNullOrWhiteSpace(name)){ name = "Unkown"; }
+
+Console.WriteLine($"Hey how is it going {name}");
+
+Console.WriteLine("What's your age my I ask?");
+string? input = Console.ReadLine();
+
+if(int.TryParse(input, out int age))
+{
+    if (age < 18)
+{
+    Console.WriteLine("Ah so you're not an adult");
+  }
+else
+    {
+        Console.WriteLine("Ah so you're an adult");
+    }
+}
+
+
+
+
+
+
+
+
+
